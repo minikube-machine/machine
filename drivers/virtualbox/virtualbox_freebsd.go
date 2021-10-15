1453ddd2499a778fb6706844c9739109612a33fa
John Hixson <john@ixsystems.com>
2021-10-15 13:15:39 -0700

    drivers/virtualbox: Follow symlinks on FreeBSD

--- drivers/virtualbox/virtualbox_freebsd.go
+++ drivers/virtualbox/virtualbox_freebsd.go
@@ -1,11 +1,18 @@
 package virtualbox
 
+import "path/filepath"
+
 func detectVBoxManageCmd() string {
 	return detectVBoxManageCmdInPath()
 }
 
 func getShareDriveAndName() (string, string) {
-	return "hosthome", "/home"
+	path, err := filepath.EvalSymlinks("/home")
+	if err != nil {
+		path = "/home"
+	}
+
+	return "hosthome", path
 }
 
 func isHyperVInstalled() bool {
