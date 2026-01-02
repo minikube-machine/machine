02049ed9728b94eec98ab9aefbe55b3b2065b661
Anders F Björklund <anders.f.bjorklund@gmail.com>
2026-01-02 10:15:43 +0100

    Move struct type to separate file

--- libmachine/host/migrate_v1_v2.go
+++ libmachine/host/migrate_v1_v2.go
@@ -4,41 +4,7 @@ import (
 	"path/filepath"
 
 	"github.com/docker/machine/libmachine/auth"
-	"github.com/docker/machine/libmachine/drivers"
-	"github.com/docker/machine/libmachine/engine"
-	"github.com/docker/machine/libmachine/swarm"
 )
-
-type AuthOptionsV1 struct {
-	StorePath            string
-	CaCertPath           string
-	CaCertRemotePath     string
-	ServerCertPath       string
-	ServerKeyPath        string
-	ClientKeyPath        string
-	ServerCertRemotePath string
-	ServerKeyRemotePath  string
-	PrivateKeyPath       string
-	ClientCertPath       string
-}
-
-type OptionsV1 struct {
-	Driver        string
-	Memory        int
-	Disk          int
-	EngineOptions *engine.Options
-	SwarmOptions  *swarm.Options
-	AuthOptions   *AuthOptionsV1
-}
-
-type V1 struct {
-	ConfigVersion int
-	Driver        drivers.Driver
-	DriverName    string
-	HostOptions   *OptionsV1
-	Name          string `json:"-"`
-	StorePath     string
-}
 
 func MigrateHostV1ToHostV2(hostV1 *V1) *V2 {
 	// Changed:  Put StorePath directly in AuthOptions (for provisioning),
